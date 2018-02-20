d942bbccc14479edfb4df468c08636a04af6aa92
Acher249 <jchernick2010@gmail.com>
2018-02-19 22:40:51 -0500

    Gravity Success
    Gravity Success

--- Assets/Scripts/Attractor.cs
+++ Assets/Scripts/Attractor.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attractor : MonoBehaviour {
+public class Attractor : MonoBehaviour
+{
+    const float G = 664.4f;
 
-	// Use this for initialization
-	void Start () {
-		
+    public Rigidbody rb;
+
+	void FixedUpdate ()
+    {
+        Attractor[] attractors = FindObjectsOfType<Attractor>();
+        foreach(Attractor attractor in attractors)
+        {
+            if (attractor != this)
+                Attract(attractor);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Attract (Attractor objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
 }
