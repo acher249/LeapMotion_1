715b6b304c5bb0a07a4eaaccd60b01c5aafcb1f6
Acher249 <jchernick2010@gmail.com>
2018-03-31 12:33:43 -0400

    Scale
    Scale

---
+++ Assets/Scripts/DistanceCalc.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceCalc : MonoBehaviour
+{
+    public GameObject HandLocation;
+
+    public GameObject Plane;
+
+    void Update()
+    {
+        int divider = 8;
+
+        var distance = Vector3.Distance(HandLocation.transform.position, Plane.transform.position);
+
+        var planeScaleX = Plane.transform.localScale;
+        planeScaleX = Plane.transform.localScale = new Vector3(distance/divider, distance/divider, distance/divider);
+
+        //transform.localScale += new Vector3(0.1F, 0, 0);
+
+        //Debug.Log("distance = " + distance);
+    }
+}
